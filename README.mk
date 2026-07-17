563e2e09a68a1af5f05bb0f677f3132ae54d286f
Eduardo H. Cardoso <101907529+OsloFrits@users.noreply.github.com>
2026-07-17 12:23:50 -0300

    Add Ideia inicial ao README.mk

---
+++ README.mk
@@ -0,0 +1,3 @@
+O plano inicial era apenas um projeto simples de alarme para entender algumas bibliotecas em Java. Porem acabei decidindo fazer um programa para me ajudar com a procrastinação.
+
+O problema agora é fazer ele.
