bd471131c88eca609c5987cf30419ecc2c711a0b
Oslo <eduardofabulam11@gmail.com>
2026-08-31 10:32:43 -0300

    Mudança no README.mk

--- README.mk
+++ README.mk
@@ -1,3 +1,4 @@
 O plano inicial era apenas um projeto simples de alarme para entender algumas bibliotecas em Java. Porem acabei decidindo fazer um programa para me ajudar com a procrastinação.
 
-O problema agora é fazer ele.
+O projeto está prosseguindo a passos lentos e quase parados, pois ainda preciso decidir, sobre como implemento certos metodos e definir as responsabilidades de forma correta.
+Tentando obdecer o SOLID e atualmente tenho falhado nisso.
